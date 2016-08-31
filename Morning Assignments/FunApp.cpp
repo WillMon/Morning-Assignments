2021e33509a9ae3a03a4adcdbe22356f5416762f
WillMon <w.montero34@gmail.com>
2016-08-31 13:11:50 -0500

    Will Mon 3rd Commit 03/31/2016
    Placed What made a planet in the constructer

--- Morning Assignments/FunApp.cpp
+++ Morning Assignments/FunApp.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <array>
-int AttackPerHit(int attck, int hits) {
-	return attck * hits;
-}
-
-void main() {
-	int (*p)(int, int);
-	 p = AttackPerHit;
-
-	 int FunArry[5];
-	 for (int i = 0; i < 5;i++) {
-		FunArry[i] = (*p)((20 * 5 * i), i);
-		std::cout << (FunArry[i]) << std::endl;
-		
-	 }
-	 system("pause");
-	return;
-}
+// Assignment COMPELETED in 08/30/2016
+//int AttackPerHit(int attck, int hits) {
+//	return attck * hits;
+//}
+//
+//void main() {
+//	int (*p)(int, int);
+//	 p = AttackPerHit;
+//
+//	 int FunArry[5];
+//	 for (int i = 0; i < 5;i++) {
+//		FunArry[i] = (*p)((20 * 5 * i), i);
+//		std::cout << (FunArry[i]) << std::endl;
+//		
+//	 }
+//	 system("pause");
+//	return;
+//}
